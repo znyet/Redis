b46bcb6959fd64423393027065d7276cad160ba5
叶涛 <821023977@qq.com>
2019-08-24 13:56:33 +0800

    1

---
+++ 驱动/ServiceStack.Redis/RedisHelper.cs
@@ -0,0 +1,29 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class RedisHelper
+    {
+        public static PooledRedisClientManager prcm = new PooledRedisClientManager("localhost"); 
+    }
+}
+
+
+
+//localhost
+//127.0.0.1:6379
+//redis://localhost:6379
+//password @localhost:6379
+//clientid:password @localhost:6379
+//redis://clientid:password@localhost:6380?ssl=true&db=1
+
+
+//using (var conn = RedisHelper.prcm.GetClient())
+//{
+                
+//}
