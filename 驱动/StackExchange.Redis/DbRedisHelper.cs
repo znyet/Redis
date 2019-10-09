e25d2c4ac7d2fc0e555be141af07eeb5137f316e
叶涛 <821023977@qq.com>
2019-10-09 14:33:00 +0800

    1

---
+++ 驱动/StackExchange.Redis/DbRedisHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using StackExchange.Redis;
+
+namespace Common.DbUtils
+{
+    public class DbRedisHelper
+    {
+        public static string ConnectionString;
+
+        public static string Ip;
+        public static int Port;
+
+        private static readonly object _locker = new object();
+        private static ConnectionMultiplexer _redis;
+
+
+        public static ConnectionMultiplexer GetConn()
+        {
+            if (_redis == null || !_redis.IsConnected)
+            {
+                lock (_locker)
+                {
+                    if (_redis != null)
+                        return _redis;
+                    _redis = ConnectionMultiplexer.Connect(ConnectionString); ;
+                    return _redis;
+                }
+            }
+
+            return _redis;
+        }
+
+        public static IEnumerable<RedisKey> GetAllKeys(int db = 0)
+        {
+            var conn = GetConn();
+            var endPoints = conn.GetEndPoints();
+            var server = conn.GetServer(endPoints[0]);
+            return server.Keys(db);
+        }
+
+
+        public static IDatabase GetDb(int db = 0, object asyncState = null)
+        {
+            return GetConn().GetDatabase(db, asyncState);
+        }
+
+
+        public static void CheckRedis()
+        {
+            while (true)
+            {
+                try
+                {
+                    GetDb().StringGet("ashdjasdkjasdhka");
+                    break;
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(3000);
+                }
+            }
+        }
+
+
+    }
+}
